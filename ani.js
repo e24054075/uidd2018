ee75f8f17e998e175269429536a48d1c32184840
e24054075 <37132224+e24054075@users.noreply.github.com>
2018-04-09 21:00:07 +0900

    Update ani.js

--- ani.js
+++ ani.js
@@ -1,11 +1,27 @@
-var point = 0;
-var c_time = 60;
-var toolbarc = 0;
-var clickt = [0,0,0,0,0,0];
-var salt = [false,false,false,false,false,false];
+var $point = 0;
+var $c_time = 60;
+var $toolbarc = 0;
+var $clickt = [0,0,0,0,0,0];
+var $salt = [false,false,false,false,false,false];
 
-$(document).ready(function(){
-$("#toolbar").click(function(){
+function move(){
     $("#toolbarcircle").animate({height:"472px",width:"472px"});
-  });
+}
+function close(){
+    $("#toolbarcircle").animate({height:"0px",width:"0px"});
+}
+$(document).ready(function(){
+    $("#toolbar").click(function(){
+    if($toolbarc)
+    {
+        move();
+        $toobarc = 1;
+    }
+    else
+    {
+        close();
+        $toobarc = 0;
+    }
+    });
+    
 });
