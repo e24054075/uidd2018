da1abc8485e1f06a82cdd2ef901a681edfc57168
e24054075 <37132224+e24054075@users.noreply.github.com>
2018-04-09 23:25:30 +0900

    Update ani.js

--- ani.js
+++ ani.js
@@ -1,8 +1,8 @@
 var point = 0;
 var c_time = 60;
 var poolc = [1,1,1,1,1,1];
-var salt = [false,false,false,false,false,false];
 var tool = 0;
+var temp = 0;
 
 function move(){
     $("#toolbarcircle").animate({height:"472px",width:"472px"});
@@ -14,28 +14,44 @@ function usetool(i){
 	$("#tool"+i).click(function(){
 		tool=i;
 		close();
-		$("#time").text(tool);
     	});
 }
-
+function pool(i,j){
+	$("#p"+i+"s"+j).click(function(){
+		for(var w = 1;w <= 5;w++)
+		{
+			if(w == j + 1)
+			{	
+				temp = j+1;
+				$("#p"+i+"s"+temp).show();
+			}
+			else
+			{
+				$("#p"+i+"s"+w).hide();
+			}
+		}
+	});
+}
 function timecount(){
 	c_time = 60;
 	timer = setInterval(function(){
 		c_time--;
 		$("#time").text("0:"+c_time);
-		if (c_time == 0)
-		{	
-			clearInterval(timer);
-		}
 	},1000);
 }
 $(document).ready(function(){
+    for(var i = 1;i<=6;i++)
+    {
+	    for(var j = 1:j <= 5;j++)
+		pool(i,j);
+    }
     $("#toolbar").click(function(){
         move();
     });
     $("#toolbar2").click(function(){
         close();
     });
+    timecount();
     for(var i = 1;i<=5;i++)
 	usetool(i);
 });
