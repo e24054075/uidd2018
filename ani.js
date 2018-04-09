b4d6df5cc068806d30f0bdb5975d4de1a0b26718
e24054075 <37132224+e24054075@users.noreply.github.com>
2018-04-09 21:44:07 +0900

    Update ani.js

--- ani.js
+++ ani.js
@@ -4,6 +4,17 @@ var $toolbarc = 0;
 var $clickt = [0,0,0,0,0,0];
 var $salt = [false,false,false,false,false,false];
 
+function timecount(){
+	c_time = 60;
+	timer = setInterval(function(){
+		c_time--;
+		$("#time").text("0:"+c_time);
+		if (c_time == 0)
+		{	
+			clearInterval(timer);
+		}
+	},1000);
+}
 function move(){
     $("#toolbarcircle").animate({height:"472px",width:"472px"});
 }
@@ -14,4 +25,5 @@ $(document).ready(function(){
     $("#toolbar").click(function(){
         move();
     });
+    timecount();
 });
