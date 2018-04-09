310743b514565198564beefbc3fbbe0f258083fe
e24054075 <37132224+e24054075@users.noreply.github.com>
2018-04-09 15:20:30 +0900

    Update ani.js

--- ani.js
+++ ani.js
@@ -4,4 +4,10 @@ var toolbarc = 0;
 var clickt = [0,0,0,0,0,0];
 var salt = [false,false,false,false,false,false];
 
-    
+$(document).ready(function(){
+$("#toolbar").click(function(){
+    $("#toolbarcircle").animate("width: 100px;
+  height: 100px;
+  -webkit-border-radius: 100px 0 0 0;");
+  });
+});
